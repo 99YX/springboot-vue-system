0a377c187e2ad27e8101ad929c58c2234e4afb08
yanixn <2896399920@qq.com>
2022-11-30 21:36:30 +0800

    SwaggerConfig修改空格

--- src/main/java/com/yanxin/study/config/SwaggerConfig.java
+++ src/main/java/com/yanxin/study/config/SwaggerConfig.java
@@ -22,6 +22,7 @@ public class SwaggerConfig {
      * 通过select()函数返回一个ApiSelectorBuilder实例,用来控制哪些接口暴露给Swagger来展现，
      * 本例采用指定扫描的包路径来定义指定要建立API的目录。
      *
+     *
      * @return
      */
     @Bean
